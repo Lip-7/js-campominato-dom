d6284ce1b51e35e36269037a1cfcb07eb5dfdfef
Lip-7 <zhighofficial@gmail.com>
2023-03-20 13:13:08 +0100

    init and fixes

---
+++ js/script.js
@@ -0,0 +1,107 @@
+document.getElementById('levelForm').addEventListener('submit', play)
+
+function play(e) {
+    e.preventDefault();
+    const playGround = document.getElementById('playground');
+    playGround.innerHTML = '';
+    document.querySelector('h3').innerHTML = `Select the difficulty and press Play!`;
+    const level = document.getElementById('level').value; /* easy = 100 ,medium = 81, hard = 49 */
+    let squareNumber = level == 'easy' ? 100 : level == 'medium' ? 81 : 49;
+    const nBombs = 16;
+    let userScore = 0;
+    let gameOver = false;
+    const bombAudio = new Audio('./audio/bLungo.mp3');
+    let squarePerRow = Math.sqrt(squareNumber);
+    const bombsArray = generateBombs(nBombs, squareNumber);
+    const winCondition = squareNumber - nBombs;
+    for (let i = 1; i <= squareNumber; i++){
+        const square = drawSquare(i, squarePerRow);
+        square.addEventListener('click', handleSquareEvent)
+        playGround.appendChild(square);
+    }
+    function handleSquareEvent() {
+        if (!gameOver && (!this.classList.contains('safe'))) {
+            if (bombsArray.includes(parseInt(this.innerText))){
+                if (!document.querySelector('.bomb')){bombAudio.play();}
+                this.classList.add('bomb');
+                document.querySelector('h3').innerHTML = `You have achieved a score of ${userScore}`;
+                gameOver = true;
+                showAllBombs(bombsArray);
+            }else{
+                this.classList.add('safe');
+                userScore ++;
+                this.style.animation = 'boing 0.4s ease-in-out';
+                if (userScore == winCondition){
+                    document.querySelector('h3').innerHTML = `You Won!!! How did you do it?!`;
+                }else{
+                    document.querySelector('h3').innerHTML = `Your score is: ${userScore}`;
+                }
+            }
+        }
+    }
+    
+}
+
+/* 
+    for (let i = 1; i <= squareNumber; i++){
+        const square = drawSquare(i, squarePerRow);
+        square.addEventListener('click', handleSquareEvent)
+        playGround.appendChild(square);
+    }
+    function handleSquareEvent() {
+        if (!gameOver && (!this.classList.contains('safe'))) {
+            if (bombsArray.includes(parseInt(this.innerText))){
+                if (!document.querySelector('.bomb')){bombAudio.play();}
+                this.classList.add('bomb');
+                document.querySelector('h3').innerHTML = `You have achieved a score of ${userScore}`;
+                gameOver = true;
+                showAllBombs(bombsArray);
+            }else{
+                this.classList.add('safe');
+                userScore ++;
+                this.style.animation = 'boing 0.4s ease-in-out'
+                if (userScore == winCondition){
+                    document.querySelector('h3').innerHTML = `You Won!!! How did you do it?!`;
+                }else{
+                    document.querySelector('h3').innerHTML = `Your score is: ${userScore}`;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+*/
+
+
+/* 
+for (let i = 1; i <= squareNumber; i++){
+        const square = drawSquare(i, squarePerRow);
+        if (bombsArray.indexOf(i) >= 0) {
+            square.addEventListener('click', () => {
+                if (!document.querySelector('.bomb')){bombAudio.play();}
+                square.classList.add('bomb');
+                const userResult = userScore;
+                const allSquares = document.querySelectorAll('.square')
+                clickAll(allSquares);
+                document.querySelector('h3').innerHTML = `You have achieved a score of ${userResult}`;
+            })
+        }else{
+            square.addEventListener('click', () => {
+                if (!this.classList.contains('safe')){
+                    square.classList.add('safe');
+                    if (!document.querySelector('.bomb')){
+                        userScore ++
+                        document.querySelector('h3').innerHTML = `Your score is: ${userScore}`
+                        square.style.animation = 'boing 0.4s ease-in-out'
+                    }else{
+                        square.style.backgroundColor = 'var(--primary-color)'
+                    }
+                }
+            })
+        }
+        playGround.appendChild(square);
+    }
+*/
---
+++ js/utility.js
@@ -0,0 +1,83 @@
+function switchVisibility(value1,value2){
+    const content1 = document.getElementById(value1);
+    const content2 = document.getElementById(value2);
+    content1.classList.add('d-none');
+    content2.classList.remove('d-none');
+    setTimeout(() => {
+        content2.style.opacity = 1;
+      }, 10);
+};
+function giveVisibility(value){
+  const content = document.getElementById(value);
+  content.classList.remove('d-none');
+  setTimeout(() => {
+    content.style.opacity = 1;
+  }, 10);
+}
+function preventSubmitRefresh(form){
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+} )
+};
+function randomNumber(max) {
+  return Math.round(Math.random() * (max - 1)) + 1;
+};
+function generateLi(listIngredients, listToAppend, tohide, toshow) {
+let iterable = 0;
+const ulDestination = document.getElementById(listToAppend);
+while (iterable < listIngredients.length){
+    const ingredient = document.createElement('li');
+    ingredient.innerText = listIngredients[iterable];
+    ulDestination.appendChild(ingredient);
+    iterable++;
+}
+switchVisibility(tohide, toshow)
+};
+function separe(buttons){
+  for (let i = 0; i < buttons.length; i++){
+      buttons[i].style.translate = 0;
+    }
+};
+function drawSquare(content, width){
+  const square = document.createElement('div');
+  square.classList.add('square');
+  square.style.width = `calc(100% / ${width})`;
+  square.style.height = `calc(100% / ${width})`;
+  square.innerHTML = content;
+  return square;
+}
+function generateBombs(nBombs, squareNumber){
+  const bombsArray = []
+  while (bombsArray.length < nBombs){
+      const bomb = randomNumber(squareNumber);
+      if (!bombsArray.includes(bomb)) {
+          bombsArray.push(bomb);
+      }
+  }
+  return bombsArray;
+}
+function simulateClick(element) {
+  const event = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true
+  });
+  element.dispatchEvent(event);
+  element.dispatchEvent(new Event('removeClick'));
+}
+function clickAll (list) {
+  let alreadyClicked = ['bomb','safe']
+  for (let i = 0; i < list.length; i++) {
+      if (!alreadyClicked.includes(list[i].classList[1])) {
+          simulateClick(list[i])
+      }
+  }
+}
+function showAllBombs(bombsArray) {
+  const squares = document.querySelectorAll('.square')
+  for (let square of squares) {
+      if (bombsArray.includes(parseInt(square.innerText))) {
+          square.classList.add('bomb')
+      }
+  }
+}
